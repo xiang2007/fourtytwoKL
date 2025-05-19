1a4e40e063880272e108c774874c5a0f3f78cfd9
Wee Shou Xiang <63289858+xiang2007@users.noreply.github.com>
2025-05-19 18:09:03 +0800

    deleetd files

--- libft/srcs/ft_isalpha.c
+++
@@ -1,7 +0,0 @@
-int	ft_isalpha(char alpha)
-{
-	if ((alpha >= 'a' && alpha <= 'z') || (alpha >= 'A' && alpha <= 'Z'))
-		return (1);
-	else
-		return (0);
-}
--- libft/srcs/ft_isdigit.c
+++
@@ -1,7 +0,0 @@
-int	ft_isdigit(char digit)
-{
-	if (digit >= '0' && digit <= '9')
-		return (1);
-	else
-		return (0);
-}
