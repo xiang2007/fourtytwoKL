35b6a7d19102443f0f661a8dd12fe5962d1a5189
Wee Shou Xiang <63289858+xiang2007@users.noreply.github.com>
2025-05-21 11:47:33 +0800

    added libft.h

---
+++ Libft/libft.h
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int	ft_atoi(const char *str);
+void	ft_bzero(void *str, size_t size);
+char	*ft_strdup(const char *str);
+int	ft_isalnum(int num);
+int ft_isalpha(int alpha);
+int	ft_isascii(int c);
+int	ft_isdigit(int digit);
+int	ft_isprint(int c);
+void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t size);
+void	*ft_memcpy(void *dest, const void *src, size_t num);
+void	*ft_memmove(void *dest, const void *src, size_t num);
+void	*ft_memset(void *str, int c, size_t n);
+char	*ft_strchr(const char *src, int	c);
+char	*ft_strdup(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+int	ft_strlen(char *str);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *find, const char *to_find, size_t len);
+char	*strrchr(const char *s, int c);
+int	ft_toupper(int ch);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
